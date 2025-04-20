fa50319c7a59623bdf220624dbf069014e054d6b
Thor Eric Dueholm <thorericdueholm@gmail.com>
2025-04-20 16:18:41 +0200

    adding dist subtree v2

--- vue.config.js
+++
@@ -1,6 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  publicPath:
-    process.env.NODE_ENV === 'production' ? '/Thor-ED.github.io/' : '/',
-  transpileDependencies: true
-})
